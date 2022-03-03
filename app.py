5d27e681cbef7937586d995eaec5e18f21b47f49
parth2002g <parth2002g@gmail.com>
2022-03-03 23:11:35 +0530

    feat: reverse integer

--- app.py
+++ app.py
@@ -19,4 +19,18 @@ def put_books():
     books.append(new_books)
     return jsonify(books)
 
+@app.route('/reverse_integer', methods=['GET'])
+def reverse_integer():
+    x = int(request.args.get("integer"))
+    if x <= 0:
+        a = -1 * int(str(x * -1)[::-1])
+    if x > 0:
+        a = int(str(x)[::-1])
+    mina = -2 ** 31
+    maxa = 2 ** 31 - 1
+    if a not in range(mina, maxa):
+        return str(0)
+    else:
+        return str(a)
+
 app.run()
